3a16d865c163d2becfa20d0a7870a2a89f2ba9ab
Jackie098 <carlosmiranda19122@gmail.com>
2020-04-24 22:55:05 -0300

    adding new items in list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -13,16 +13,38 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: `Diego Fernandes`
+    });
+
+    const project = response.data;
+
+    setProjects([ ...projects, project]);
+    console.log(projects);
+  }
+
   return (
     <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      
       <SafeAreaView style={styles.container}>
         <FlatList
           data={projects}
           keyExtractor={project => project.Id}
           renderItem={({ item: project }) => (
-            <Text style={styles.projects}>{project.title}</Text>
+            <Text style={styles.project}>{project.title}</Text>
           )}
         />
+
+        <TouchableOpacity 
+          activeOpacity={0.9} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   ); 
@@ -34,9 +56,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#7159c1',
   },
 
-  projects: {
+  project: {
     color: '#FFF',
     fontSize: 20,
     fontWeight: 'bold',
+    marginTop: 10,
+    backgroundColor: '#7140c1',
+    padding: 15,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    borderBottomColor: '#FFF',
   },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 })
